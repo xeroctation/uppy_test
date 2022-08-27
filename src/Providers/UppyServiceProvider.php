60d9ef6fce82618697d40d478cf7f81aa170c034
xeroctation <umidakosimova.uk@gmail.com>
2022-08-27 14:36:10 +0500

    config publish

--- src/Providers/UppyServiceProvider.php
+++ src/Providers/UppyServiceProvider.php
@@ -16,6 +16,7 @@ class UppyServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../resources/views/components/' => base_path('resources/views/components'),
             __DIR__ . '/../View/Components/' => base_path('app/View/Components'),
+            __DIR__.'/../config/uppy.php' => config_path('uppy.php'),
         ]);
     }
 
